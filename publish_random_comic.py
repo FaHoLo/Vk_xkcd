ccf4641eb1e47ae1744f69458eb18bb401580736
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2019-07-16 22:37:06 +0300

    Increase reliability, separate tasks of functions

--- publish_random_comic.py
+++ publish_random_comic.py
@@ -9,23 +9,23 @@ def main():
     publish_new_comic_post()
 
 def publish_new_comic_post():
-    image_name, message = download_random_comic()
-    post_comic_on_wall(image_name, message)
-    os.remove(image_name)
-    
+    image_name, image_info = download_random_comic()   
+    try:
+        message = build_comic_message(image_info)
+        post_comic_on_wall(image_name, message)
+    finally:
+        os.remove(image_name)
+
 def download_random_comic():
     random_comic_number = get_random_comic_number()
     url = f'https://xkcd.com/{random_comic_number}/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
-    response = response.json()
-    img_url = response['img']
+    img_info = response.json()
+    img_url = img_info['img']
     img_name = os.path.split(img_url)[-1]   
-    title = response['title']
-    comment = response['alt']
-    message = f'{title}.\n{comment}'
     download_image(img_url, img_name)
-    return img_name, message
+    return img_name, img_info
 
 def get_random_comic_number():
     number_of_comics = get_current_number_of_comics()
@@ -45,28 +45,34 @@ def download_image(url, image_name):
     with open(f'{image_name}', 'wb') as new_file:
         new_file.write(response.content)
 
+def build_comic_message(image_info):
+    title = image_info['title']
+    comment = image_info['alt']
+    message = f'{title}.\n{comment}'
+
 def post_comic_on_wall(image_name, message):
-    VK_GROUP_ID = os.getenv('VK_GROUP_ID')
-    photo_attribs = save_to_album(image_name, VK_GROUP_ID)
+    vk_group_id = os.getenv('VK_GROUP_ID')
+    photo_attribs = save_to_album(image_name, vk_group_id)
     photo_owner_id = photo_attribs['owner_id']
     photo_id = photo_attribs['id']
     attachment = f'photo{photo_owner_id}_{photo_id}'
-    publish_post_on_wall(message, attachment, VK_GROUP_ID)
+    publish_post_on_wall(message, attachment, vk_group_id)
 
-def save_to_album(image_name, VK_GROUP_ID):
-    save_params = vk_upload_on_serv(image_name, VK_GROUP_ID)
+def save_to_album(image_name, vk_group_id):
+    save_params = vk_upload_on_serv(image_name, vk_group_id)
+    method_name = 'photos.saveWallPhoto'
     payload = {
-        'group_id': VK_GROUP_ID,
+        'group_id': vk_group_id,
         'server': save_params['server'],
         'photo': save_params['photo'],
         'hash': save_params['hash'],
     }
-    response = vk_api_request('photos.saveWallPhoto', payload)
+    response = vk_api_request(method_name, payload)
     photo_attributes = response['response'][0]
     return photo_attributes
 
-def vk_upload_on_serv(image_name, VK_GROUP_ID):
-    upload_url = vk_get_upload_url(VK_GROUP_ID)
+def vk_upload_on_serv(image_name, vk_group_id):
+    upload_url = vk_get_upload_url(vk_group_id)
     with open(f'{image_name}', 'rb') as photo:
         file = {'photo': photo}
         response = requests.post(upload_url, files=file)
@@ -74,9 +80,9 @@ def vk_upload_on_serv(image_name, VK_GROUP_ID):
     save_params = response.json()
     return save_params
 
-def vk_get_upload_url(VK_GROUP_ID):
+def vk_get_upload_url(vk_group_id):
     method_name = 'photos.getWallUploadServer'
-    payload = {'group_id': VK_GROUP_ID}
+    payload = {'group_id': vk_group_id}
     api_response = vk_api_request(method_name, payload)
     upload_url = api_response['response']['upload_url']
     return upload_url
@@ -94,15 +100,15 @@ def vk_api_request(method_name, payload):
     response.raise_for_status()
     return response.json()
 
-def publish_post_on_wall(message, attachments, VK_GROUP_ID):
-    VK_GROUP_ID = f'-{VK_GROUP_ID}'
+def publish_post_on_wall(message, attachments, vk_group_id):
+    method_name = 'wall.post'
     payload = {
-        'owner_id': VK_GROUP_ID,
+        'owner_id': f'-{vk_group_id}',
         'from_group': '1',
         'message': message,
         'attachments': attachments,        
     }
-    vk_api_request('wall.post', payload)
+    vk_api_request(method_name, payload)
 
 if __name__ == '__main__':
     main()
